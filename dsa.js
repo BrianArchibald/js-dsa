dd5b73ef7a874585c2585a18867572c2ea72396a
Brian Archibald <brianallenarchibald@gmail.com>
2021-08-31 14:51:40 -0500

    is subsequence

--- dsa.js
+++ dsa.js
@@ -205,3 +205,18 @@ function avgPair(arr, num){
 }
 
 console.log(avgPair([1,2,3],2.5))
+
+
+//  I string 1 a substring of string 2, multiple pointers
+
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
