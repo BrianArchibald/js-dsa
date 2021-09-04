ad987c6a61e861d86583ce09f61c84649efa6edf
Brian Archibald <brianallenarchibald@gmail.com>
2021-09-04 08:04:37 -0500

    max sub array

--- dsa.js
+++ dsa.js
@@ -220,3 +220,20 @@ function isSubsequence(str1, str2) {
   }
   return false;
 }
+
+// max sub array, given an array and a num, max total nums or array /// SLIDING WINDOW
+
+function maxSubarraySum(arr, num){
+  if (arr.length < num) return null;
+
+  let total = 0;
+  for (let i=0; i<num; i++){
+     total += arr[i];
+  }
+  let currentTotal = total;
+  for (let i = num; i < arr.length; i++) {
+     currentTotal += arr[i] - arr[i-num];
+     total = Math.max(total, currentTotal);
+  }
+  return total;
+}
