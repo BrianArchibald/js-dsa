361af3e6428b878dc09d978ed1201775daa15a3a
Brian Archibald <brianallenarchibald@gmail.com>
2021-09-08 11:29:45 -0500

    more recursion

--- dsa.js
+++ dsa.js
@@ -331,3 +331,16 @@ function collectOddValues(arr){
 }
 
 collectOddValues([1,2,3,4,5,6,7,8,9])
+
+// Power recursive
+function power(base, exponent){
+  if(exponent === 0) return 1;
+  return base * power(base,exponent-1);
+}
+/// RECURSION product of array
+function productOfArray(arr) {
+  if(arr.length === 0) {
+      return 1;
+  }
+  return arr[0] * productOfArray(arr.slice(1));
+}
