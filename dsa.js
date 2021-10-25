91164d63711b89845b38442c531048a57ffe3032
Brian Archibald <brianallenarchibald@gmail.com>
2021-10-25 18:56:16 -0500

    return answer and exp null

--- dsa.js
+++ dsa.js
@@ -11,7 +11,7 @@ function same(arr1, arr2){
         frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
     }
     for(let val of arr2){
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
     }
     for(let key in frequencyCounter1){
         if(!(key ** 2 in frequencyCounter2)){
@@ -54,16 +54,16 @@ function validAnagram(first, second) {
     if (first.length !== second.length) {
       return false;
     }
-  
+
     const lookup = {};
-  
+
     for (let i = 0; i < first.length; i++) {
       let letter = first[i];
       // if letter exists, increment, otherwise set to 1
       lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
     }
     console.log(lookup)
-  
+
     for (let i = 0; i < second.length; i++) {
       let letter = second[i];
       // can't find letter or letter is zero then it's not an anagram
@@ -73,7 +73,7 @@ function validAnagram(first, second) {
         lookup[letter] -= 1;
       }
     }
-  
+
     return true;
   }
 
@@ -93,7 +93,7 @@ function validAnagram(first, second) {
             else:
                 lookup[char] -= 1
         return True
-  
+
   // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
   validAnagram('anagrams', 'nagaramm')
 
@@ -102,7 +102,7 @@ function validAnagram(first, second) {
 function zeroSum(arr) {
     let left = 0
     let right = arr.length - 1
-    
+
     while (left < right) {
       let sum = arr[left] + arr[right]
       if (sum === 0) {
@@ -116,7 +116,7 @@ function zeroSum(arr) {
       }
      }
   }
-  
+
 //  console.log(zeroSum([-2, -1, 0, 1]))
 
 // Unique Values
@@ -158,22 +158,22 @@ function sameFrequency(num1, num2){
   let strNum1 = num1.toString();
   let strNum2 = num2.toString();
   if(strNum1.length !== strNum2.length) return false;
-  
+
   let countNum1 = {};
   let countNum2 = {};
-  
+
   for(let i = 0; i < strNum1.length; i++){
     countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
   }
-  
+
   for(let j = 0; j < strNum1.length; j++){
     countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
   }
-  
+
   for(let key in countNum1){
     if(countNum1[key] !== countNum2[key]) return false;
   }
- 
+
   return true;
 }
 
@@ -184,7 +184,7 @@ function areThereDuplicates() {
 }
 /////////////////////////////////////////////////////////////////////////
 //  Multiple pointers
-Given a sorted array and an average int , determine if there an average of a pair that adds up to the avg int 
+Given a sorted array and an average int , determine if there an average of a pair that adds up to the avg int
 
 function avgPair(arr, num){
   if (arr.length === 0){ return false }
@@ -244,27 +244,27 @@ function minSubArrayLen(nums, sum) {
   let start = 0;
   let end = 0;
   let minLen = Infinity;
- 
+
   while (start < nums.length) {
-    // if current window doesn't add up to the given sum then 
+    // if current window doesn't add up to the given sum then
 		// move the window to right
     if(total < sum && end < nums.length){
       total += nums[end];
 			end++;
     }
     // if current window adds up to at least the sum given then
-		// we can shrink the window 
+		// we can shrink the window
     else if(total >= sum){
       minLen = Math.min(minLen, end-start);
 			total -= nums[start];
 			start++;
-    } 
-    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+    }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
     else {
       break;
     }
   }
- 
+
   return minLen === Infinity ? 0 : minLen;
 }
 
@@ -274,7 +274,7 @@ function findLongestSubstring(str) {
   let longest = 0;
   let seen = {};
   let start = 0;
- 
+
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
     if (seen[char]) {
@@ -293,7 +293,7 @@ function findLongestSubstring(str) {
 RECURSION
 
 function sumRange(num){
-  if(num === 1) return 1; 
+  if(num === 1) return 1;
   return num + sumRange(num-1);
 }
 
@@ -310,21 +310,21 @@ function factorial(num){
 
 //  Helper method recursion
 function collectOddValues(arr){
-    
+
   let result = [];
 
   function helper(helperInput){
       if(helperInput.length === 0) {
           return;
       }
-      
+
       if(helperInput[0] % 2 !== 0){
           result.push(helperInput[0])
       }
-      
+
       helper(helperInput.slice(1))
   }
-  
+
   helper(arr)
 
   return result;
@@ -360,7 +360,7 @@ function fib(n){
     let res = capitalizeWords(array.slice(0, -1));
     res.push(array.slice(array.length-1)[0].toUpperCase());
     return res;
-   
+
   }}
 
   // RECURSION capitalize words
@@ -371,7 +371,7 @@ function fib(n){
     let res = capitalizeWords(array.slice(0, -1));
     res.push(array.slice(array.length-1)[0].toUpperCase());
     return res;
-   
+
   }
 
   // Linear serach
@@ -410,6 +410,7 @@ DFS  Time O(n) and Space is O(n)
 Iterative
 
 const depthFirstValues = (root) => {
+  if (root == null) return []
   const result = []
   const stack = [root]
   while (stack.length > 0) {
@@ -419,4 +420,5 @@ const depthFirstValues = (root) => {
     if (current.right) stack.push(current.right)
     if (current.left) stack.push(current.left)
   }
+  return result
 }
