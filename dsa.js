53ddc86c9b1766789319c321a01e08553ad0cf40
Brian Archibald <brianallenarchibald@gmail.com>
2021-10-30 11:52:08 -0500

    tree sum

--- dsa.js
+++ dsa.js
@@ -481,3 +481,32 @@ const treeIncludes = (root, target) => {
 
   return treeIncludes(root.left, target) || treeIncludes(root.right, target)
 }
+
+////////////////////////////////////////////////////////////////////////////
+//
+Tree Sum
+Recursive
+
+const treeSum = (root) => {
+  if (root == null) return 0
+  return root.val + treeSum(root.left) + treeSum(root.right)
+}
+
+Tree Sum
+Iterative
+
+const treeSum = (root) => {
+  if (root == null) return 0
+  let totalSum = 0
+
+  const queue = [ root ]
+  while (queue.length > 0) {
+    const current = queue.shift()
+    totalSum += current.val
+
+    if (current.left !== null) queue.push(current.left)
+    if (current.right !== null) queue.push(current.right)
+  }
+  return totalSum
+}
+
